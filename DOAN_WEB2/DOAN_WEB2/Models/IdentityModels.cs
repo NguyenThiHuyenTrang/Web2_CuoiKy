b65cee98a137b6a06fee5a1fd2cef0d53d6947ca
runtoyou105 <runtoyou105@gmail.com>
2017-06-06 12:39:57 +0700

    Phần đăng nhâp đang ký

--- DOAN_WEB2/DOAN_WEB2/Models/IdentityModels.cs
+++ DOAN_WEB2/DOAN_WEB2/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DOAN_WEB2.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("MobileShopConnection", throwIfV1Schema: false)
         {
         }
 
