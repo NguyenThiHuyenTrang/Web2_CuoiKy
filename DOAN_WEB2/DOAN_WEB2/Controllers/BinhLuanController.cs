24eb28ecc9fbd17d4e38b4c803fc2d7b845601ae
“runtoyou105” <runtoyou105@gmail.com>
2017-06-06 13:24:10 +0700

    bình luận cho sản phẩm

---
+++ DOAN_WEB2/DOAN_WEB2/Controllers/BinhLuanController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DOAN_WEB2.Models.BUS;
+
+namespace DOAN_WEB2.Controllers
+{
+    public class BinhLuanController : Controller
+    {
+        // GET: BinhLuan
+        [Authorize]
+        public ActionResult Create(String MaSanPham, String NoiDung)
+        {
+
+            BinhLuanBUS.Them(MaSanPham, User.Identity.GetUserId(), User.Identity.Name, NoiDung);
+            return RedirectToAction("Details", "Shop", new { Id = MaSanPham });
+        }
+
+        public ActionResult Index(string MaSP)
+        {
+            ViewBag.MaSP = MaSP;
+            return View(BinhLuanBUS.DanhSach(MaSP));
+        }
+    }
+}
---
+++ DOAN_WEB2/DOAN_WEB2/Models/BUS/BinhLuanBUS.cs
@@ -0,0 +1,30 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOAN_WEB2.Models.BUS
+{
+    public class BinhLuanBUS
+    {
+        public static void Them(string MaSP, string MaTaiKhoan, string TenTaiKhoan, string NoiDung)
+        {
+            using (var sql = new MobileShopConnectionDB())
+            {
+                BinhLuan binhLuan = new BinhLuan();
+                binhLuan.MaSP = MaSP;
+                binhLuan.MaTaiKhoan = MaTaiKhoan;
+                binhLuan.NoiDung = NoiDung;
+                sql.Execute("INSERT INTO [dbo].[BinhLuan]([MaSP],[MaTaiKhoan],[TenTaiKhoan],[NoiDung]) VALUES (@0,@1,@2,@3)", MaSP, MaTaiKhoan, TenTaiKhoan, NoiDung);
+            }
+        }
+        public static IEnumerable<BinhLuan> DanhSach(String MaSP)
+        {
+            using (var sql = new MobileShopConnectionDB())
+            {
+                return sql.Query<BinhLuan>(" select * from BinhLuan  where MaSP = @0 ORDER BY Ngay DESC", MaSP);
+            }
+        }
+    }
+}
